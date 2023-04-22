f554844b0e4e601ad7c8e31442021c2bf29bf4c0
alexeydoronin <neonixys@mail.ru>
2023-04-22 15:02:27 +0300

    Lesson 37

--- to_do_list/goals/admin.py
+++ to_do_list/goals/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
-
-from to_do_list.goals.models import GoalCategory, Goal
+from to_do_list.goals.models import GoalCategory, Goal, Board
 
 
 @admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "created", "updated")
-    search_fields = ("title", "user")
-    list_filter = ("is_deleted",)
+    list_display = ('title', 'user', 'created', 'updated')
+    search_fields = ('title',)
+    list_filter = ('is_deleted',)
 
 
 @admin.register(Goal)
-class GoalAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "created", "updated")
-    search_fields = ("title", "user", "description")
-    list_filter = ("status", "priority")
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created', 'updated')
+    search_fields = ('title', 'discription')
+    list_filter = ('status', 'priority')
+
+
+@admin.register(Board)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'updated')
+    search_fields = ('title', )
+    list_filter = ('is_deleted',)
