8228f6f9aa66e4332d17746db3334499ec984d05
alexeydoronin <neonixys@mail.ru>
2023-05-12 11:19:12 +0300

    Lesson 38

--- to_do_list/bot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'to_do_list.bot'
--- to_do_list/bot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from to_do_list.bot import views
-
-urlpatterns = [
-    path('verify', views.VerificationView.as_view(), name='verify-bot'),
-]
